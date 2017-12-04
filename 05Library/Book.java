9b296fae78d77490a4b1eedd5868a1cba6faae44
shuxinwu <swu10@stuy.edu>
2017-12-03 22:45:14 -0500

    add

---
+++ 05Library/Book.java
@@ -0,0 +1,33 @@
+public class Book{
+  private String author;
+  private String title;
+  private String ISBN;
+
+  public Book(){ }
+
+  public Book(String a, String b, String c){
+    author = a;
+    title = b;
+    ISBN = c;
+  }
+
+  public String getAuthor(){ return author; }
+  public String getTitle(){ return title; }
+  public String getISBN(){ return ISBN; }
+
+  public void setAuthor(String a){
+    author = a;
+  }
+
+  public void setTitle(String b){
+    title = b;
+  }
+
+  public void setISBN(String c){
+    ISBN = c;
+  }
+
+  public String toString(){
+    return title + "\t " + author + "\t" + ISBN;
+  }
+}
