671fe9c06dd411349fb020bed4708e624e7aca9a
shuxinwu <swu10@stuy.edu>
2017-12-18 23:08:21 -0500

    constructor

--- 09GUI/Convert.java
+++ 09GUI/Convert.java
@@ -2,7 +2,38 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class Convert{
+public class Convert extends JFrame implements ActionListener{
+  private Container pane;
+  private JButton b1, b2;
+  private JTextField t;
+
+  public void actionPerformed(ActionEvent e){
+    String s = e.getActionCommand();
+
+  }
+
+  public Convert(){
+    this.setTitle("Converting...");
+    this.setSize(400, 500);
+    this.setLocation(100, 100);
+    this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+    pane = this.getContentPane();
+    pane.setLayout(new FlowLayout());
+    b1 = new JButton("F to C");
+    b2 = new JButton("C to F");
+    t = new JTextField(15);
+
+    b1.addActionListener(this);
+    b2.addActionListener(this);
+    t.addActionListener(this);
+
+    pane.add(t);
+    pane.add(b1);
+    pane.add(b2);
+
+  }
+
   public static double CtoF(double t){
     return t * 9.0 / 5.0 + 32.0;
   }
