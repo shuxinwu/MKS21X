bbd8cf23bead2e042630a6673689a4da98e48501
shuxinwu <swu10@stuy.edu>
2018-01-03 19:45:33 -0500

    next hasnext remove

---
+++ 06SuperArrayIterator/SuperArrayIterator.java
@@ -0,0 +1,33 @@
+import java.util.Iterator;
+
+public class SuperArrayIterator implements Iterator<String>{
+  private SuperArray a;
+  private int current;
+
+  public SuperArrayIterator(SuperArray b){
+    a = b;
+    current = 0;
+  }
+
+  // true if iteration has more elements
+  public boolean hasNext(){
+    return current < a.size();
+  }
+
+  // next element in iteration
+  public String next(){
+    if (hasNext()){
+      current++;
+    }
+    else{
+      System.exit(0);
+    }
+    return a.get(current - 1);
+  }
+
+  // unsupported
+  public void remove(){
+    throw new UnsupportedOperationException();
+  }
+
+}
